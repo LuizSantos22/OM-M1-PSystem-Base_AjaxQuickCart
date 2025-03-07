2e40c5636a00896db4465c1bb74d7801e0263235
LuizSantos22 <83614706+LuizSantos22@users.noreply.github.com>
2025-03-07 12:00:14 -0300

    Update Layerjs.php

--- app/code/community/PSystem/AjaxQuickCart/Block/Layerjs.php
+++ app/code/community/PSystem/AjaxQuickCart/Block/Layerjs.php
@@ -3,16 +3,15 @@
  * @category   PSystem
  * @package    PSystem_AjaxQuickCart
  * @author     Pascal System <info@pascalsystem.pl>
- * @version    1.0.2
+ * @version    1.0.3
  */
-
 /**
  * Pascal Quick Cart Ajax Layer JavaScripts
  * 
  * @category   PSystem
  * @package    PSystem_AjaxQuickCart
  * @author     Pascal System <info@pascalsystem.pl>
- * @version    1.0.2
+ * @version    1.0.3
  */
 class PSystem_AjaxQuickCart_Block_Layerjs extends Mage_Core_Block_Abstract {
 /**
@@ -20,14 +19,15 @@ class PSystem_AjaxQuickCart_Block_Layerjs extends Mage_Core_Block_Abstract {
  * 
  * @return string
  */
-	public function _toHtml() {
+	protected function _toHtml() {
 		$selector = Mage::getStoreConfig('psajaxquickcart/quickcart/selectorsingle');
-		if (empty($selector))
+		if (empty($selector)) {
 			$selector = ".add-to-cart .btn-cart";
+		}
 		
 		$html = '<script type="text/javascript">'."\n";
-		$html.= 'PS.AjaxQuickCart.initLayerQuick(\''.$selector.'\');';
-		$html.= '</script>'."\n";
+		$html .= 'PS.AjaxQuickCart.initLayerQuick('.json_encode($selector).');';
+		$html .= '</script>'."\n";
 		
 		return $html;
 	}
