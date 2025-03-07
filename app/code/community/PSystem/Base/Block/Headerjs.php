a583f1bdab5c65260f66600bf7afec11569e77d6
LuizSantos22 <83614706+LuizSantos22@users.noreply.github.com>
2025-03-07 12:20:43 -0300

    Update Headerjs.php

--- app/code/community/PSystem/Base/Block/Headerjs.php
+++ app/code/community/PSystem/Base/Block/Headerjs.php
@@ -5,7 +5,6 @@
  * @author     Pascal System <info@pascalsystem.pl>
  * @version    1.1.2
  */
-
 /**
  * Pascal Catalog Ajax config block
  * 
@@ -14,24 +13,56 @@
  * @author     Pascal System <info@pascalsystem.pl>
  * @version    1.1.2
  */
-class PSystem_Base_Block_Headerjs extends Mage_Core_Block_Abstract {
-/**
- * Prepare additional js to head
- * 
- * @return string
- */
-	public function _toHtml() {
-		if (Mage::getStoreConfig('psbase/quickimages/disable'))
-			return '';
-		
-		if (!$selector = Mage::getStoreConfig('psbase/quickimages/selector')) {
-			$selector = 'a.product-image';
-		}
-		
-		//outer div - ie7,ie8 - bug
-		$html = '<div style="display:none;">&nbsp;<script type="text/javascript">';
-		$html.= 'PS.catalogQuickViewImage.init(\''.$selector.'\');';
-		$html.= '</script></div>';
-		return $html;
-	}
+class PSystem_Base_Block_Headerjs extends Mage_Core_Block_Abstract
+{
+    /**
+     * Prepare additional js to head
+     * 
+     * @return string
+     */
+    protected function _toHtml()
+    {
+        if (Mage::getStoreConfig('psbase/quickimages/disable')) {
+            return '';
+        }
+        
+        if (!$selector = Mage::getStoreConfig('psbase/quickimages/selector')) {
+            $selector = 'a.product-image';
+        }
+        
+        // Properly escape the selector for JavaScript
+        $selector = $this->escapeJs($selector);
+        
+        // Use proper JavaScript inclusion method
+        $html = '<div style="display:none;">';
+        $html .= '<script type="text/javascript">//<![CDATA[';
+        $html .= 'document.observe("dom:loaded", function() {';
+        $html .= '    if (typeof PS !== "undefined" && PS.catalogQuickViewImage) {';
+        $html .= '        PS.catalogQuickViewImage.init(\'' . $selector . '\');';
+        $html .= '    }';
+        $html .= '});';
+        $html .= '//]]></script>';
+        $html .= '</div>';
+        
+        return $html;
+    }
+    
+    /**
+     * Escape string for JavaScript
+     *
+     * @param string $string
+     * @return string
+     */
+    public function escapeJs($string)
+    {
+        if ($string === null) {
+            return '';
+        }
+        
+        return str_replace(
+            ['\\', '\'', '"', "\r", "\n", "\t", '<', '>', '&'],
+            ['\\\\', '\\\'', '\\"', '', '', '', '\\u003C', '\\u003E', '\\u0026'],
+            $string
+        );
+    }
 }
