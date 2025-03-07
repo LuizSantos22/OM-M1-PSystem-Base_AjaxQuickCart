a596747b27522ba7717ee4ad093cd1fefff010ab
LuizSantos22 <83614706+LuizSantos22@users.noreply.github.com>
2025-03-07 12:07:28 -0300

    Update Renderer.php

--- app/code/community/PSystem/AjaxQuickCart/Block/Item/Renderer.php
+++ app/code/community/PSystem/AjaxQuickCart/Block/Item/Renderer.php
@@ -3,30 +3,59 @@
  * @category   PSystem
  * @package    PSystem_AjaxQuickCart
  * @author     Pascal System <info@pascalsystem.pl>
- * @version    1.0.2
+ * @version    1.0.3
  */
-
 /**
  * Pascal Quick Cart Ajax item layer renderer
  * 
  * @category   PSystem
  * @package    PSystem_AjaxQuickCart
  * @author     Pascal System <info@pascalsystem.pl>
- * @version    1.0.2
+ * @version    1.0.3
  */
-class PSystem_AjaxQuickCart_Block_Item_Renderer extends Mage_Checkout_Block_Cart_Item_Renderer {
-/**
- * Get item delete url
- *
- * @return string
- */
-	public function getDeleteUrl() {
-		return $this->getUrl(
-			'checkout/cart/delete',
-			array(
-				'id'=>$this->getItem()->getId(),
-				Mage_Core_Controller_Front_Action::PARAM_NAME_URL_ENCODED => $this->helper('core/url')->getEncodedUrl(Mage::getUrl('checkout/cart/index'))
-			)
-		);
-	}
+class PSystem_AjaxQuickCart_Block_Item_Renderer extends Mage_Checkout_Block_Cart_Item_Renderer
+{
+    /**
+     * Get item delete url
+     *
+     * @return string
+     */
+    public function getDeleteUrl()
+    {
+        $params = array(
+            'id' => $this->getItem()->getId(),
+            Mage_Core_Controller_Front_Action::PARAM_NAME_URL_ENCODED => $this->helper('core/url')->getEncodedUrl(Mage::getUrl('checkout/cart/index'))
+        );
+        
+        // Add form key for CSRF protection if available
+        if (method_exists($this, 'getFormKey')) {
+            $params['form_key'] = $this->getFormKey();
+        }
+        
+        return $this->getUrl('checkout/cart/delete', $params);
+    }
+    
+    /**
+     * Get form key
+     *
+     * @return string
+     */
+    public function getFormKey()
+    {
+        return Mage::getSingleton('core/session')->getFormKey();
+    }
+    
+    /**
+     * Override to allow proper caching with CSRF protection
+     *
+     * @return array
+     */
+    public function getCacheKeyInfo()
+    {
+        $info = parent::getCacheKeyInfo();
+        $info[] = $this->getItem()->getId();
+        $info[] = $this->getFormKey();
+        
+        return $info;
+    }
 }
