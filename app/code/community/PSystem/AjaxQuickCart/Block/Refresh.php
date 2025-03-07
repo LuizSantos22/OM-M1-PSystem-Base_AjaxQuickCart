21dd016bee4300e9b0f01368c17543a3df410091
LuizSantos22 <83614706+LuizSantos22@users.noreply.github.com>
2025-03-07 12:04:49 -0300

    Update Refresh.php

--- app/code/community/PSystem/AjaxQuickCart/Block/Refresh.php
+++ app/code/community/PSystem/AjaxQuickCart/Block/Refresh.php
@@ -3,28 +3,45 @@
  * @category   PSystem
  * @package    PSystem_AjaxQuickCart
  * @author     Pascal System <info@pascalsystem.pl>
- * @version    1.0.2
+ * @version    1.0.3
  */
-
 /**
  * Pascal Quick Cart Ajax refresh data JavaScripts
  * 
  * @category   PSystem
  * @package    PSystem_AjaxQuickCart
  * @author     Pascal System <info@pascalsystem.pl>
- * @version    1.0.2
+ * @version    1.0.3
  */
-class PSystem_AjaxQuickCart_Block_Refresh extends Mage_Core_Block_Abstract {
-/**
- * Get html code
- * 
- * @return string
- */
-	public function _toHtml() {
-		$html = '<script type="text/javascript">'."\n";
-		$html.= 'PS.AjaxQuickCart.refresh(\''.$this->getUrl('ajaxquickcart/viewcart/refresh').'\');';
-		$html.= '</script>'."\n";
-		
-		return $html;
-	}
+class PSystem_AjaxQuickCart_Block_Refresh extends Mage_Core_Block_Abstract
+{
+    /**
+     * Get html code
+     * 
+     * @return string
+     */
+    public function _toHtml()
+    {
+        $html = '<script type="text/javascript">' . "\n";
+        $html .= 'PS.AjaxQuickCart.refresh(' . json_encode($this->getUrl('ajaxquickcart/viewcart/refresh')) . ');';
+        $html .= '</script>' . "\n";
+        
+        return $html;
+    }
+    
+    /**
+     * Override to allow csrf protection compatibility
+     *
+     * @return array
+     */
+    public function getCacheKeyInfo()
+    {
+        return [
+            'BLOCK_TPL',
+            Mage::app()->getStore()->getId(),
+            $this->getTemplateFile(),
+            'template' => $this->getTemplate(),
+            $this->getUrl('ajaxquickcart/viewcart/refresh')
+        ];
+    }
 }
