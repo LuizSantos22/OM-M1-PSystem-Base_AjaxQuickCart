3bf82c1631b8004512bc8f2a52878ae5594d2ad2
LuizSantos22 <83614706+LuizSantos22@users.noreply.github.com>
2025-03-07 11:29:32 -0300

    Update Data.php

--- app/code/community/PSystem/Base/Helper/Data.php
+++ app/code/community/PSystem/Base/Helper/Data.php
@@ -3,16 +3,15 @@
  * @category   PSystem
  * @package    PSystem_Base
  * @author     Pascal System <info@pascalsystem.pl>
- * @version    1.1.2
+ * @version    1.1.3
  */
-
 /**
  * Pascal Base Helper
  * 
  * @category   PSystem
  * @package    PSystem_Base
  * @author     Pascal System <info@pascalsystem.pl>
- * @version    1.1.2
+ * @version    1.1.3
  */
 class PSystem_Base_Helper_Data extends Mage_Core_Helper_Abstract {
 }
